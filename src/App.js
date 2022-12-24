8eed0eb371c0ad3fa012176372274927c4a5a470
Oguzhan Kayapinar <oguzhankayapinar@gmail.com>
2022-12-24 15:00:53 +0300

    V1-1-1_createbook

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import BookCreate from './components/BookCreate'
+
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log(' budur:', title)
+
+
+  }
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
+}
+
+export default App;
